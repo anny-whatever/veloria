38906154e0df476f34153e5fe157e16066d4c306
anny-whatever <devscryptcomp@gmail.com>
2025-03-15 11:55:20 +0530

    Added demo env and base URL according to hosted server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create axios instance with default config
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://api.veloria.in/api",
   headers: {
     "Content-Type": "application/json",
   },
