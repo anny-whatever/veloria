0dc98fffc71af248491ab113bbeb2e0706e99566
anny-whatever <devscryptcomp@gmail.com>
2025-04-08 10:10:30 +0530

    Allowed veloria.in on vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["9344-49-36-101-231.ngrok-free.app"],
+    allowedHosts: ["veloria.in", "www.veloria.in"],
   },
   build: {
     rollupOptions: {
