831910eab62ceefb6fd62d1a0ec58e7d204269fe
anny-whatever <devscryptcomp@gmail.com>
2025-04-08 09:47:46 +0530

    Updated cors

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ const corsOptions = {
     "http://localhost:3000",
     "http://localhost:5173",
   ],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET, POST, PUT, DELETE, OPTIONS"],
+  allowedHeaders: ["Content-Type, Authorization"],
   credentials: true,
   optionsSuccessStatus: 200,
 };
